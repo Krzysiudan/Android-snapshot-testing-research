f4ca4c71ecd74c37bcbbcc5e7eb422e787a48fd0
Krzysztof <krzysiudan@gmail.com>
2024-01-20 15:55:27 +0100

    Prepare test suit with same tests for the every library under test.

--- dialogs/shot/src/androidTest/java/com/example/road/to/effective/snapshot/testing/dialogs/shot/parameterized/TestItems.kt
+++ dialogs/shot/src/androidTest/java/com/example/road/to/effective/snapshot/testing/dialogs/shot/parameterized/TestItems.kt
@@ -34,6 +34,15 @@ enum class HappyPathTestItem(val deleteItem: DeleteDialogTestItem) {
             dialogWidth = DialogWidth.NORMAL,
         )
     ),
+    NORMAL_NIGHT(
+        DeleteDialogTestItem(
+            bulletTexts = listOf(R.string.largest, R.string.middle, R.string.shortest),
+            dialogWidth = DialogWidth.NORMAL,
+            viewConfig = ViewConfigItem(
+                uiMode = UiMode.NIGHT,
+            ),
+        )
+    ),
 }
 
 enum class UnhappyPathTestItem(val deleteItem: DeleteDialogTestItem) {
@@ -52,6 +61,26 @@ enum class UnhappyPathTestItem(val deleteItem: DeleteDialogTestItem) {
             viewConfig = ViewConfigItem(
                 fontSize = FontSize.HUGE,
                 uiMode = UiMode.NIGHT,
+            ),
+            bulletTexts = repeatedItem(7, R.string.largest),
+            dialogWidth = DialogWidth.NARROW,
+        ),
+    ),
+    SPACIOUS_DAY(
+        DeleteDialogTestItem(
+            viewConfig = ViewConfigItem(
+                fontSize = FontSize.SMALL,
+                uiMode = UiMode.DAY,
+            ),
+            bulletTexts = listOf(R.string.shortest),
+            dialogWidth = DialogWidth.WIDE,
+        ),
+    ),
+    SUFFOCATED_DAY(
+        DeleteDialogTestItem(
+            viewConfig = ViewConfigItem(
+                fontSize = FontSize.HUGE,
+                uiMode = UiMode.DAY,
             ),
             bulletTexts = repeatedItem(7, R.string.largest),
             dialogWidth = DialogWidth.NARROW,
